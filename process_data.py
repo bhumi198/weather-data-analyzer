bb0184c049d626bd00f760224b9be4d6b72439d0
daisy0602 <bhumipeco@gmail.com>
2026-03-19 17:31:17 +0530

    anomaly detection(basic)

--- process_data.py
+++ process_data.py
@@ -1,6 +1,14 @@
 import pandas as pd
 from datetime import datetime
 
+def detect_anomaly(temp):
+    if temp > 35:
+        return "High Temperature Alert"
+    elif temp < 10:
+        return "Low Temperature Alert"
+    else:
+        return "Normal"
+
 def process_data(raw_data):
     processed = {
         "timestamp": datetime.now(),
@@ -8,9 +16,9 @@ def process_data(raw_data):
         "feels_like": raw_data["main"]["feels_like"],
         "humidity": raw_data["main"]["humidity"],
         "pressure": raw_data["main"]["pressure"],
-        "weather": raw_data["weather"][0]["description"]
+        "weather": raw_data["weather"][0]["description"],
+        "alert": detect_anomaly(raw_data["main"]["temp"])
     }
 
     df = pd.DataFrame([processed])
     return df
-
